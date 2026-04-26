a528d48710802dcfc965e4c635bb63d29bd66d5c
Akshat Bansal <akshat13bansal@gmail.com>
2026-04-26 23:41:22 +0530

    added drill suggestion  and updated UI  in ML module

--- ml_service/court_command_ML/app.py
+++ ml_service/court_command_ML/app.py
@@ -1,23 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import pickle
-import json
 import numpy as np
+import pickle
 import os
 
 app = Flask(__name__)
 CORS(app)
 
+# LOAD MODEL
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Load model
 with open(os.path.join(BASE_DIR, "models", "win_predictor.pkl"), "rb") as f:
     model_data = pickle.load(f)
 
 model = model_data["model"]
 scaler = model_data["scaler"]
+# PREDICTION ROUTE
 
-# API
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
@@ -39,19 +38,42 @@ def predict():
             data['away_reb']
         ])
 
+        # Model logic (difference)
         diff = home - away
         diff_scaled = scaler.transform([diff])
-
         prob = model.predict_proba(diff_scaled)[0][1]
 
+        winner = "HOME TEAM" if prob > 0.5 else "AWAY TEAM"
+        losing_team = "AWAY TEAM" if prob > 0.5 else "HOME TEAM"
+
+        weaker = away if prob > 0.5 else home
+        # DRILL LOGIC
+      
+        drills = []
+
+        if weaker[0] < 0.45:
+            drills.append("Low shooting efficiency → Shot selection + contested shooting drills")
+
+        if weaker[1] < 0.75:
+            drills.append("Poor FT → 100 pressure free throws daily")
+
+        if weaker[2] < 0.35:
+            drills.append("Weak 3PT → Corner 3 repetition drills")
+
+        if weaker[3] < 24:
+            drills.append("Low assists → Ball movement drills (3-pass rule)")
+
+        if weaker[4] < 42:
+            drills.append("Weak rebounding → Box-out drills")
+
         return jsonify({
-            "winner": "HOME WIN" if prob > 0.5 else "AWAY WIN",
-            "confidence": f"{round(prob*100,1)}%"
+            "winner": winner,
+            "losing_team": losing_team,
+            "drills": drills
         })
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 400
-
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
